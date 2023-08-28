2ad9afffaa956972adf60e87a38402f5e249db41
amssilva92 <amssilva.92@gmail.com>
2023-08-28 21:24:45 +0100

    Second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Tempini } from './components/TempiniApp/Tempini';
+
+function App() {
+  return (
+    <div className="App">
+      <Tempini/>
+    </div>
+  );
+}
+
+export default App;
